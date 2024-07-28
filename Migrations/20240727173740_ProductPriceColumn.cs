27c2af15a1feec1764fad72061e5884acc60bb55
Aurus Soares <neiven131@gmail.com>
2024-07-28 20:38:35 -0300

    feat: Add migrations files

---
+++ Migrations/20240727173740_ProductPriceColumn.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductAPI.Migrations
+{
+    public partial class ProductPriceColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ProductPrice",
+                table: "Products",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductPrice",
+                table: "Products");
+        }
+    }
+}
