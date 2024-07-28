9e26b7fe97b6f582fea00f337ca71b65dca1b7b7
Aurus Soares <neiven131@gmail.com>
2024-07-28 20:37:21 -0300

    feat: Add context class

---
+++ Contexts/ProductContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductAPI.Entities;
+
+namespace ProductAPI.Contexts
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
